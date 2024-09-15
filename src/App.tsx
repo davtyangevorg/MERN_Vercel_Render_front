c71c32d143cedb9e7d21f4fd046058ddd55d8c50
Gevorg <gevorgdavtyan99@mail.ru>
2024-09-15 19:02:56 +0400

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState<{ name: string }[]>([]);
+
+  const getUsers = async () => {
+    try {
+      const response = await fetch(
+        "https://mern-vercel-render-back-backend-service.onrender.com/users"
+      );
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
@@ -28,8 +41,17 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <button onClick={getUsers}>get users</button>
+        <h1>users</h1>
+        <ul>
+          {users.map((user, index) => (
+            <li key={index}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
